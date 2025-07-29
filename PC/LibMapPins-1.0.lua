a44a49d78e055bdbd749d0b982850cd6725db1f5
Sharlikran <danielhmpdx16@gmail.com>
2025-07-28 17:50:38 -0700

    Version: 1.0.47

--- PC/LibMapPins-1.0.lua
+++ PC/LibMapPins-1.0.lua
@@ -1,35 +1,71 @@
+--[[
 -------------------------------------------------------------------------------
 -- LibMapPins-1.0
 -------------------------------------------------------------------------------
+-- Core library Copyright (c) 2014, 2015 Ales Machat (Garkin)
 --
--- Copyright (c) 2014, 2015 Ales Machat (Garkin)
+-- Current maintainer: Sharlikran (contributions since 2020-05-08)
+-- Previous maintainers: AssemblerManiac, Ayantir, Fyrakin, Sensi, Scootworks
 --
--- Permission is hereby granted, free of charge, to any person
--- obtaining a copy of this software and associated documentation
--- files (the "Software"), to deal in the Software without
--- restriction, including without limitation the rights to use,
--- copy, modify, merge, publish, distribute, sublicense, and/or sell
--- copies of the Software, and to permit persons to whom the
--- Software is furnished to do so, subject to the following
--- conditions:
+-- ----------------------------------------------------------------------------
+-- Core portions of this library are licensed under the following terms:
 --
--- The above copyright notice and this permission notice shall be
--- included in all copies or substantial portions of the Software.
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
 --
--- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
--- EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
--- OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
--- NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
--- HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
--- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
--- FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
--- OTHER DEALINGS IN THE SOFTWARE.
+-- The above copyright notice and this permission notice shall be included in
+-- all copies or substantial portions of the Software.
 --
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+-- THE SOFTWARE.
+--
+-- ----------------------------------------------------------------------------
+-- Contributions by Sharlikran (starting 2020-05-08) are licensed under the
+-- BSD 3-Clause License:
+--
+-- Redistribution and use in source and binary forms, with or without
+-- modification, are permitted provided that the following conditions are met:
+--
+-- 1. Redistributions of source code must retain the above copyright notice,
+--    this list of conditions and the following disclaimer.
+--
+-- 2. Redistributions in binary form must reproduce the above copyright notice,
+--    this list of conditions and the following disclaimer in the documentation
+--    and/or other materials provided with the distribution.
+--
+-- 3. Neither the name of the author "Sharlikran" nor the names of previous
+--    maintainers may be used to endorse or promote products derived from this
+--    software without specific prior written permission.
+--
+-- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+-- AND ANY EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED. IN NO EVENT SHALL THE
+-- COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+-- WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING IN ANY WAY OUT
+-- OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--
+-- Maintainer Notice:
+-- Redistribution of this software outside of ESOUI.com (including Bethesda.net
+-- or other platforms) is discouraged unless authorized by the current
+-- maintainer. While the original MIT license permits redistribution with proper
+-- attribution and license inclusion, uncoordinated platform uploads may cause
+-- version fragmentation or compatibility issues. Please respect the intent of
+-- the maintainer and the integrity of the ESO addon ecosystem.
 -------------------------------------------------------------------------------
+]]
+
 local lib = {}
 
 lib.name = "LibMapPins-1.0"
-lib.version = 10046
+lib.version = 10047
 lib.filters = {}
 lib.mapGroup = "pve"
 lib.pinManager = ZO_WorldMap_GetPinManager()
@@ -358,6 +394,38 @@ function lib:SetLayoutKey(pinType, key, data)
   end
 end
 
+-------------------------------------------------------------------------------
+-- lib:SetFilterTooltipCreator(pinType, filterTooltipCreator)
+-------------------------------------------------------------------------------
+-- Assigns a tooltip function or static string to be used when hovering
+-- over the checkbox entry in the pin filter menu.
+--
+-- pinType:              pinTypeId or pinTypeString
+-- filterTooltipCreator: function returning tooltip text or a string directly
+--
+-- If a string is passed, it will be automatically wrapped in a function.
+--
+-- example:
+-- LibMapPins:SetFilterTooltipCreator("pinType_Skyshards", "Shows skyshard locations")
+-- LibMapPins:SetFilterTooltipCreator("pinType_Bosses", function() return GetDynamicTooltipText() end)
+-------------------------------------------------------------------------------
+function lib:SetFilterTooltipCreator(pinType, filterTooltipCreator)
+  if not filterTooltipCreator then return end
+  local filterFunction
+  if type(filterTooltipCreator) == "function" then
+    filterFunction = filterTooltipCreator
+  elseif type(filterTooltipCreator) == "string" then
+    filterFunction = function() return filterTooltipCreator end
+  end
+
+  local pinTypeId = GetPinTypeId(pinType)
+  if pinTypeId then
+    local pinData = self.pinManager.customPins[pinTypeId]
+    if pinData then
+      pinData.filterTooltipCreator = filterFunction
+    end
+  end
+end
 -------------------------------------------------------------------------------
 -- lib:SetClickHandlers(pinType, LMB_handler, RMB_handler)
 -------------------------------------------------------------------------------
